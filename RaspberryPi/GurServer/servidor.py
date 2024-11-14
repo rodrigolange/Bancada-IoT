65a3fdc680550b4e2b2ed2dd99581c856bbe9b6d
Rodrigo Lange <52110364+rodrigolange@users.noreply.github.com>
2024-11-13 22:12:06 -0300

    Add files via upload

--- RaspberryPi/GurServer/servidor.py
+++ RaspberryPi/GurServer/servidor.py
@@ -24,25 +24,12 @@ weather_thread = threading.Thread(target=weather_forecast_task)
 weather_thread.daemon = True
 weather_thread.start()
 
-class SilentHTTPRequestHandler(BaseHTTPRequestHandler):
-    # Override the log_message method to silence logging
-    def log_message(self, format, *args):
-        return  # Do nothing, effectively silencing the log messages
-
 
 def handler_factory(*args, **kwargs):
     return ChirpStackHandler(forecast, listaTransmissoesNodos, *args, **kwargs)
 
-
-# Modify the HTTPServer to use handler_factory, but with silent logging
-class CustomHTTPServer(HTTPServer):
-    def __init__(self, server_address, handler_factory):
-        self.RequestHandlerClass = handler_factory  # Use the handler_factory for dynamic handlers
-        super().__init__(server_address, SilentHTTPRequestHandler)  # Silence logs for all requests
-
-
 # Set up and start the HTTP server
 server_address = ('', 5000)  # Listen on all network interfaces, port 8080
-httpd = CustomHTTPServer(server_address, handler_factory)
+httpd = HTTPServer(server_address, handler_factory)
 print("HTTP server running on port 5000...")
 httpd.serve_forever()
